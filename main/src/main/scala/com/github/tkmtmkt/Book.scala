d2abf945349d6c78ed955ce49fbcee050a76e92c
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-07-21 20:41:53 +0900

    POFファイルを追加（まだ動かない）

---
+++ main/src/main/scala/com/github/tkmtmkt/Book.scala
@@ -0,0 +1,14 @@
+package com.github.tkmtmkt
+
+import com.tangosol.io.pof.annotation.Portable
+import com.tangosol.io.pof.annotation.PortableProperty
+
+@Portable
+class Book
+{
+  @PortableProperty(1)
+  var title:String = _
+
+  @PortableProperty(2)
+  var author:String = _
+}
--- main/src/main/scala/com/github/tkmtmkt/Main.scala
+++ main/src/main/scala/com/github/tkmtmkt/Main.scala
@@ -6,6 +6,8 @@ import com.tangosol.net.NamedCache;
 object Main extends App
 {
   val hoge = CacheFactory.getCache("hoge")
-
-  println("Hello, workld")
+  val book = new Book
+  book.title = "Scalaで学ぶ関数脳入門"
+  book.author = "株式会社テクノロジックアート"
+  hoge.put("ISBN978-4-7741-4436-8", book)
 }
