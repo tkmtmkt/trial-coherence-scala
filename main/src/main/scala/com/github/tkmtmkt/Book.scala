e0ac94819df5a883b32cc1cdb3681f68ec48f342
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-07-23 23:24:10 +0900

    package-dist追加
    ≪参考≫
    http://xerial.org/scala-cookbook/recipes/2012/06/28/create-a-scala-project/

--- main/src/main/scala/com/github/tkmtmkt/Book.scala
+++ main/src/main/scala/com/github/tkmtmkt/Book.scala
@@ -3,10 +3,9 @@ package com.github.tkmtmkt
 import com.tangosol.io.pof.annotation.Portable
 import com.tangosol.io.pof.annotation.PortableProperty
 
-@serializable
 @SerialVersionUID(123L)
 @Portable
-class Book
+class Book extends Serializable
 {
   @PortableProperty(1)
   var title:String = _
--- project/Build.scala
+++ project/Build.scala
@@ -1,24 +1,35 @@
 import sbt._
 import Keys._
 
-object MyBuild extends Build {
+object ProjectBuild extends Build
+{
+  override lazy val settings = super.settings ++
+    Seq(
+      version := "0.1-SNAPSHOT",
+      organization := "com.github.tkmtmkt",
+      description := "",
+      scalaVersion := "2.10.2",
+      scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
+      resolvers += Resolver.mavenLocal,
+      retrieveManaged := true,
+      libraryDependencies ++= Seq(
+        "junit" % "junit" % "4.10" % "test",
+        "org.specs2" %% "specs2" % "1.11" % "test",
+        "org.mockito" % "mockito-core" % "1.9.0" % "test"
+      )
+    )
 
-  lazy val root: Project = Project("root", file("."), aggregate = nonRoots)
   lazy val nonRoots = projects.filter(_ != root).map(p => LocalProject(p.id))
+  lazy val root: Project = Project("root", file("."), aggregate = nonRoots)
 
-  lazy val main = templateProject("main", file("main"))
-
-  def templateProject(nameString: String, path: File) = Project(nameString, path) settings(
-    version := "0.1-SNAPSHOT",
-    organization := "com.github.tkmtmkt",
-    description := "",
-    scalaVersion := "2.10.2",
-    resolvers += Resolver.mavenLocal,
-    libraryDependencies ++= Seq(
-      "com.oracle.coherence" % "coherence" % "3.7.1.7",
-      "junit" % "junit" % "4.10" % "test",
-      "org.specs2" %% "specs2" % "1.11" % "test",
-      "org.mockito" % "mockito-core" % "1.9.0" % "test"
+  lazy val main = Project("main", file("main"), settings = Defaults.defaultSettings ++
+    Seq(PackageTask.packageDistTask) ++
+    PackageTask.distSettings ++
+    Seq(
+      libraryDependencies ++= Seq(
+        "org.codehaus.plexus" % "plexus-classworlds" % "2.4",
+        "com.oracle.coherence" % "coherence" % "3.7.1.7"
+      )
     )
   )
 }
