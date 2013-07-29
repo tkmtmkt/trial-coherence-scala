24b09bc4fcd5c04eb91a77366a7f42adb580df79
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-07-29 09:15:39 +0900

    ivyリポジトリをビルドツリー内に作成するようにした。

--- bin/sbt.bat
+++
@@ -1,2 +0,0 @@
-@set SCRIPT_DIR=%~dp0
-@java -Xmx1024M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+@set SCRIPT_DIR=%~dp0
+
+set SBT_OPTS=-Xms1024M -Xmx1024M -Xss1M
+set SBT_OPTS=%SBT_OPTS% -XX:MaxPermSize=200M -XX:ReservedCodeCacheSize=60M
+set SBT_OPTS=%SBT_OPTS% -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit
+set SBT_OPTS=%SBT_OPTS% -Dfile.encoding=UTF-8
+set SBT_OPTS=%SBT_OPTS% -Dbuild.home=%SCRIPT_DIR%
+set SBT_OPTS=%SBT_OPTS% -Dsbt.ivy.home="%SCRIPT_DIR%repository"
+set SBT_OPTS=%SBT_OPTS% -Dsbt.boot.properties="file:/%SCRIPT_DIR%sbt/sbt.boot.properties"
+
+set SBT_JAR="%SCRIPT_DIR%sbt\sbt-launch.jar"
+
+java %JAVA_OPTS% %SBT_OPTS% -jar %SBT_JAR% %*
